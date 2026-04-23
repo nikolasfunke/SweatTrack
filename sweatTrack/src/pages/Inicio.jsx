01d17ac518fd32a6cf3d04b6de6fa24b99967984
Luigi Battistini <lubattis19@gmail.com>
2026-04-23 16:21:07 -0300

    style: vídeo animado como bg

--- sweatTrack/src/pages/Inicio.jsx
+++ sweatTrack/src/pages/Inicio.jsx
@@ -2,15 +2,30 @@ import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import logoFull from "../assets/logo_sweatTrack.svg";
 import logoIcon from "../assets/logo.svg";
+import bgVideo from "../assets/bg_video_pre.mp4";
+import bgStatic from "../assets/bg_static.jpeg";
 
 function Inicio() {
   const [showForm, setShowForm] = useState(false);
 
   return (
-    <div className="min-h-screen flex flex-col items-center pt-30 relative overflow-hidden">
+    <div className="min-h-screen flex flex-col items-center pt-36 relative overflow-hidden">
       {/*  FUTURO FUNDO ANIMADO */}
-      /
-      <div className="absolute inset-0 bg-gradient-to-br from-zinc-900 via-zinc-800 to-black -z-10" />
+      {/* <div className="absolute inset-0 bg-gradient-to-br from-zinc-900 via-zinc-800 to-black -z-10" /> */}
+      <div className="absolute inset-0 -z-20">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="w-full h-full object-cover blur-[2px] scale-120 brightness-75 opacity-80"
+        >
+          <source src={bgVideo} type="video/mp4" />
+        </video>
+      </div>
+
+      <div className="absolute inset-0 bg-gradient-to-br from-black/40 via-black/10 via-red/30 to-black/40 -z-10" />
+
       {/* LOGO */}
       <AnimatePresence>
         <motion.img
@@ -21,9 +36,13 @@ function Inicio() {
             y: showForm ? -20 : 0, // leve ajuste quando abre o form
           }}
           transition={{ duration: 0.4 }}
-          className="w-86 mx-auto mb-32 drop-shadow-md"
+          className="w-86 mx-auto mb-46 drop-shadow-md"
         />
       </AnimatePresence>
+
+      
+
+
       {/* CONTEÚDO */}
       <div className="w-full max-w-xs px-6">
         <AnimatePresence mode="wait">
@@ -35,6 +54,7 @@ function Inicio() {
               exit={{ opacity: 0, y: -40 }}
               className="flex flex-col gap-4"
             >
+              
               <button
                 onClick={() => setShowForm(true)}
                 className="w-full py-3 rounded-full bg-[#830202] text-white font-semibold shadow-lg hover:bg-red-800 transition"
@@ -49,20 +69,21 @@ function Inicio() {
           ) : (
             <motion.form
               key="form"
-              initial={{ opacity: 0, y: 40 }}
-              animate={{ opacity: 1, y: -20 }}
+              initial={{ opacity: 0, y: 30 }}
+              animate={{ opacity: 1, y: -60 }}
               exit={{ opacity: 0, y: -40 }}
               className="flex flex-col gap-4"
             >
+
               <input
                 placeholder="Email"
-                className="px-4 py-3 rounded-lg bg-white/10 backdrop-blur-md border border-white/20 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+                className="px-4 py-3 rounded-lg bg-white/10 backdrop-blur-md border border-white/20 text-white placeholder:text-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
               />
 
               <input
                 type="password"
                 placeholder="Senha"
-                className="px-4 py-3 rounded-lg bg-white/10 backdrop-blur-md border border-white/20 text-white placeholder:text-gray-300 focus:outline-none focus:ring-2 focus:ring-red-500"
+                className="px-4 py-3 rounded-lg bg-white/10 backdrop-blur-md border border-white/20 text-white placeholder:text-gray-700 focus:outline-none focus:ring-2 focus:ring-red-500"
               />
 
               <button className="w-full py-3 rounded-full bg-[#830202] text-white font-semibold hover:bg-red-700 transition">
@@ -72,7 +93,7 @@ function Inicio() {
               <button
                 type="button"
                 onClick={() => setShowForm(false)}
-                className="text-sm text-gray-400 mt-2"
+                className="text-sm text-gray-900/90 mt-2"
               >
                 ← Voltar
               </button>

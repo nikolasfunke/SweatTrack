4d8f95026ef3e9b074624e862be48b34a5e6e93e
Nikolas Funke <nikfunke1@gmail.com>
2026-05-06 10:49:59 -0300

    feat: add react router dom

--- sweatTrack/src/App.jsx
+++ sweatTrack/src/App.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import Inicio from './pages/Inicio'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <Login />,
-    
-    <Inicio/>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/inicio" element={<Inicio />} />
+    </Routes>
   );
 }
 

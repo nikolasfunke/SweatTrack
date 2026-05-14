a146a717e31f1c256fc7b4c05df4a50d0626bc99
matheusanderson18 <matheusamoraes2006@gmail.com>
2026-05-14 17:50:39 -0300

    feat: cria tela perfil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,22 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import Inicio from './pages/Inicio'
+import Dashboard from './pages/Dashboard'
 import PreSessao from './pages/PreSessao'
 import DuranteSessao from './pages/DuranteSessao'
 import PosSessao from './pages/PosSessao'
+import Perfil from './pages/Perfil'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
       <Route path="/PreSessao" element={<PreSessao />} />
       <Route path="/DuranteSessao" element={<DuranteSessao />} />
       <Route path="/PosSessao" element={<PosSessao />} />
+      <Route path="/Perfil" element={<Perfil />} />
     </Routes>
   );
 }
--- frontend/src/pages/PosSessao.jsx
+++ frontend/src/pages/PosSessao.jsx
@@ -282,6 +282,7 @@ SALVAR NO PRONTUÁRIO
 {/* NAVBAR */}
 <nav className="fixed bottom-0 left-0 right-0 max-w-md mx-auto bg-[#F8F5F5] border-t border-black/[0.03] flex justify-around items-center pt-3 pb-5 px-2 rounded-t-[30px] shadow-[0_-10px_30px_rgba(0,0,0,0.03)] z-50">
 
+{/* INÍCIO */}
 <button className="flex flex-col items-center gap-1 text-gray-400">
 
 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
@@ -295,7 +296,8 @@ Início
 
 </button>
 
-<button className="flex flex-col items-center gap-1 text-[#E3002B] bg-red-50 px-5 py-2 rounded-2xl -mt-2">
+{/* MONITORAR */}
+<button className="flex flex-col items-center gap-1 text-gray-400">
 
 <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
 <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z"></path>
@@ -307,7 +309,8 @@ Monitorar
 
 </button>
 
-<button className="flex flex-col items-center gap-1 text-gray-400">
+{/* ANALISES ATIVO */}
+<button className="flex flex-col items-center gap-1 text-[#E3002B] bg-red-50 px-5 py-2 rounded-2xl -mt-2">
 
 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
 <path d="M18 20V10"></path>
@@ -321,6 +324,7 @@ Análises
 
 </button>
 
+{/* PERFIL */}
 <button className="flex flex-col items-center gap-1 text-gray-400">
 
 <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">

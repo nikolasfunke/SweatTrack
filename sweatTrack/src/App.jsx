b5dbec510b6379e56efd2864a8b1fb92d7beeef6
thalesxvt <thalesxvt5@gmail.com>
2026-05-14 12:27:18 -0300

    feat: implementa tela dashboard (inicio)

--- sweatTrack/src/App.jsx
+++ sweatTrack/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import Inicio from './pages/Inicio'
+import Dashboard from './pages/Dashboard'
 import PreSessao from './pages/PreSessao'
 import DuranteSessao from './pages/DuranteSessao'
 import PosSessao from './pages/PosSessao'
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Inicio />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
       <Route path="/PreSessao" element={<PreSessao />} />
       <Route path="/DuranteSessao" element={<DuranteSessao />} />
       <Route path="/PosSessao" element={<PosSessao />} />

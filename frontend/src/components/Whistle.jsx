992c90fbc22a668036831e426dffd479209c48de
rafaelscheidtt <rafaelscheidt@outlook.com>
2026-05-28 09:35:57 -0300

    fix: ajustando pastas

--- frontend/src/components/Whistle.jsx
+++
@@ -1,9 +0,0 @@
-import { GiWhistle } from "react-icons/gi";
-
-function Whistle({ className = "" }) {
-  return (
-    <GiWhistle size={24} className={className} />
-  );
-}
-
-export default Whistle;
